15fd697d3b7df97f8ca30d6d6a4e0bd4161403ef
kavi k <kaviarasan14@gmail.com>
2020-09-30 14:14:47 +0100

    kavi intel site

---
+++ js/rendering-with-properties.js
@@ -0,0 +1,22 @@
+/**
+ * Created by kavi on 02/04/2016.
+ */
+$(document).ready(function() {
+
+    /*Login Page Validations*/
+
+    // $('#loginSubmit').click(function () {
+    //     loader();
+    // });
+
+    // var pro = load the kavi-inte.properties
+
+    $('company-tile-top').val(pro.getPropertyValue("company-title"));
+
+    $('company-tile-top').val("Kavi Intel");
+    $('company-tile-top').val("Kavi Intel");
+    $('company-tile-top').val("Kavi Intel");
+    $('company-tile-top').val("Kavi Intel");
+    $('company-tile-top').val("Kavi Intel");
+
+}
